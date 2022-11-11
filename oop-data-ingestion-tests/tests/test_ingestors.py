dd20dfac671a78d51ff1c2ffd1d29ec060f812c0
Willian Barbosa <willian@qindin.com.br>
2022-11-10 23:11:01 -0300

    refactoring tests using fixture

--- oop-data-ingestion-tests/tests/test_ingestors.py
+++ oop-data-ingestion-tests/tests/test_ingestors.py
@@ -4,6 +4,21 @@ from ingestors import DataIngestor
 from writers import DataWriter
 
 import datetime
+import pytest
+
+
+'''
+@fixture behaviors like @Setup for the tests. Usually is the last of function's parameters
+'''
+@pytest.fixture
+@patch("ingestors.DataIngestor.__abstractmethods__", set())
+def data_ingestor_fixture():
+    return DataIngestor(
+            coins=['TEST', 'HOW'],
+            default_start_date=datetime.date(2022,10,30),
+            writer=DataWriter
+        )
+
 
 '''
 We're going to need @patch here because we're testing a method from an Abstract Class (ABC)
@@ -11,37 +26,26 @@ and that class needs the @abstractmethod ingest to be implemented.
 '''
 @patch("ingestors.DataIngestor.__abstractmethods__", set())
 class TestIngestors:
-    def test_checkpoint_filename(self):
-        actual = DataIngestor(
-            coins=['TEST', 'HOW'],
-            default_start_date=datetime.date(2022,10,30),
-            writer=DataWriter
-        )._checkpoint_filename
+    def test_checkpoint_filename(self, data_ingestor_fixture):
+        actual = data_ingestor_fixture._checkpoint_filename
         expected = "DataIngestor.checkpoint"
         assert actual == expected
 
 
-    def test_load_checkpoint_no_checkpoint(self):
-        actual = DataIngestor(
-            coins=['TEST', 'HOW'],
-            default_start_date=datetime.date(2022, 10, 30),
-            writer=DataWriter
-        )._load_checkpoint()
+    def test_load_checkpoint_no_checkpoint(self, data_ingestor_fixture):
+        actual = data_ingestor_fixture._load_checkpoint()
         expected = datetime.date(2022, 10, 30)
         assert actual == expected
 
     '''
     Here we're going to use @patch to mock the python method 'open'
 
+
     @patch with mock -> we need to declare this mock in function's test arguments
     '''
     @patch("builtins.open", new_callable=mock_open, read_data="2022-9-15")
-    def test_load_checkpoint_existing_checkpoint(self, mock):
-        actual = DataIngestor(
-            coins=['TEST', 'HOW'],
-            default_start_date=datetime.date(2022, 10, 31),
-            writer=DataWriter
-        )._load_checkpoint()
+    def test_load_checkpoint_existing_checkpoint(self, mock, data_ingestor_fixture):
+        actual = data_ingestor_fixture._load_checkpoint()
         expected = datetime.date(2022, 9, 15)
         assert actual == expected
 
@@ -51,12 +55,8 @@ class TestIngestors:
     the ._write_checkpoint() - avoiding the creation of .checkpoint file.
     '''
     @patch("ingestors.DataIngestor._write_checkpoint", return_value=None)
-    def test_update_checkpoint_checkpoint_updated(self, mock):
-        data_ingestor = DataIngestor(
-            coins=['TEST', 'HOW'],
-            default_start_date=datetime.date(2020, 10, 31),
-            writer=DataWriter
-        )
+    def test_update_checkpoint_checkpoint_updated(self, mock, data_ingestor_fixture):
+        data_ingestor = data_ingestor_fixture
         data_ingestor._update_checkpoint(value=datetime.date(2022, 1, 1))
         actual = data_ingestor._checkpoint
         expected = datetime.date(2022, 1, 1)
@@ -64,12 +64,8 @@ class TestIngestors:
     
 
     @patch("ingestors.DataIngestor._write_checkpoint", return_value=None)
-    def test_update_checkpoint_checkpoint_written(self, mock):
-        data_ingestor = DataIngestor(
-            coins=['TEST', 'HOW'],
-            default_start_date=datetime.date(2020, 10, 31),
-            writer=DataWriter
-        )
+    def test_update_checkpoint_checkpoint_written(self, mock, data_ingestor_fixture):
+        data_ingestor = data_ingestor_fixture
         data_ingestor._update_checkpoint(value=datetime.date(2022, 1, 1))
         mock.assert_called_once()
 
@@ -80,11 +76,7 @@ class TestIngestors:
     '''
     @patch("builtins.open", new_callable=mock_open, read_data="2022-10-16")
     @patch("ingestors.DataIngestor._checkpoint_filename", return_value="foobar.checkpoint")
-    def test_write_checkpoint(self, mock_checkpoint_file_name, mock_open_file):
-        data_ingestor = DataIngestor(
-            coins=['TEST', 'HOW'],
-            default_start_date=datetime.date(2020, 10, 31),
-            writer=DataWriter
-        )
+    def test_write_checkpoint(self, mock_checkpoint_file_name, mock_open_file, data_ingestor_fixture):
+        data_ingestor = data_ingestor_fixture
         data_ingestor._write_checkpoint()
         mock_open_file.assert_called_with(mock_checkpoint_file_name, 'w')
